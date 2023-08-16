c0360698cf9ec1196a792e36c6fad1f49ad67555
Carter Bryden <carter@approximated.app>
2023-08-16 00:07:30 +0000

    Setting check_origin to use :conn for prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -40,6 +40,7 @@ if config_env() == :prod do
 
   config :blogz, BlogzWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: :conn,
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
